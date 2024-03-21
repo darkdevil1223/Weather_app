4b9e5aff2fe5a1d431b0dbd91eb3c3de6f216b62
sarthak gaikwad <sarthakrg14@gmail.com>
2024-03-21 16:16:23 +0530

    backend done

--- weather_pro/app/app.py
+++ weather_pro/app/app.py
@@ -1,20 +1,36 @@
 import requests
-api_key = '67b3f37d64723aff149e9e34129f2b06'
+# import math
+def inp(cit):
+    global city
+    city = cit
 
-city = input("Enter city: ")
+def wed_app():
 
-wether = requests.get(
-    f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&APPID={api_key}")
+    api_key = '67b3f37d64723aff149e9e34129f2b06'
 
-# print(wether.json())
+    # city = input("Enter city: ")
+    # city = 'Mumbai'
 
-wed = wether.json()['weather'][0]['description']
-temp = wether.json()['main']['temp']
-hum = wether.json()['main']['humidity']
-loc = wether.json()['name']
-cl = (temp - 32)*5/9
+    wether = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&APPID={api_key}")
+
+    # print(wether.json())
+
+    wed = wether.json()['weather'][0]['description']
+    spd = wether.json()['wind']['speed']
+    temp = wether.json()['main']['temp']
+    hum = wether.json()['main']['humidity']
+    loc = wether.json()['name']
+    tem = (temp - 32)*5/9
+
+    cl = round(tem, 2)
 
 # print(wed, cl, hum)
-print(f"Todays weather is {wed}")
-print(f"Tempratre is {cl}'C")
-print(f"humidity is {hum}")
+    print(f"Todays weather is {wed}")
+    print(f"Tempratre is {cl}'C")
+    print(f"humidity is {hum}")
+    print(f"Location is {loc}")
+    print(f"wind speed is {spd}")
+
+
+    return [wed,temp,hum,loc,cl,spd]
--- weather_pro/app/views.py
+++ weather_pro/app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from .app import wed_app, inp
 
 def index(request):
-    # return HttpResponse("this is home page ")
+    # return HttpResponse("this is tp")
+    if request.method == 'POST':
+        cit = str(request.POST['city'])
+        inp(cit)
+        tp = wed_app()
+        wed = tp
+        return render(request, 'index.html', {'weather': wed[0], 'temp': wed[4],'hum':wed[2], 'loc':wed[3], 'spd':wed[5]})
     return render(request, 'index.html')
